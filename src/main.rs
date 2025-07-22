7536d93a94bc81711264299c4beac00290fd6e52
JaydenHuang555 <JaydenkanaHuang@outlook.com>
2025-07-22 10:34:32 -0700

    s

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 mod info;
 
+use std::env;
+use crate::info::engine::Engine;
+//  [u32; 3]
 fn main() {
+   let _args: Vec<String> = env::args().collect();
+   let mut red: [u32; 3] = [0, 0, 0];
+   let mut blue: [u32; 3] = [0, 0, 0];
+   let engine: Engine = Engine::new(red, blue); 
 }
